d49e98b5d812fd44a4dd57faabc8eb0ecaa3b37c
JC <kjamevor@unomaha.edu>
2021-04-11 20:46:46 -0500

    Updates

--- forum/settings.py
+++ forum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e^@vxiyp%=yqjmk9mzh-&fdh&z1yu^bflytg!z6g*9=lfdamjg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
